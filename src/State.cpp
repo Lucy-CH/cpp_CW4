3163f630d424c6b900104065e65f8dc412297cca
psylc7 <psylc7@exmail.nottingham.ac.uk>
2020-04-15 19:14:01 +0100

    save before I break it

---
+++ src/State.cpp
@@ -0,0 +1,8 @@
+//
+//  State.cpp
+//  SDL2_App
+//
+//  Created by Le Cheng on 15/04/2020.
+//
+
+#include "State.hpp"
---
+++ src/State.hpp
@@ -0,0 +1,13 @@
+//
+//  State.hpp
+//  SDL2_App
+//
+//  Created by Le Cheng on 15/04/2020.
+//
+
+#ifndef State_hpp
+#define State_hpp
+
+#include <stdio.h>
+
+#endif /* State_hpp */
