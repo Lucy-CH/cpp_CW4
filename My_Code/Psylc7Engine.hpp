24f90355209ffa539dd0d7dd37eb495e8d60996a
psylc7 <psylc7@exmail.nottingham.ac.uk>
2020-04-15 17:47:39 +0100

    added custom engine

---
+++ My_Code/Psylc7Engine.hpp
@@ -0,0 +1,13 @@
+//
+//  Psylc7Engine.hpp
+//  SDL2_App
+//
+//  Created by Le Cheng on 15/04/2020.
+//
+
+#ifndef Psylc7Engine_hpp
+#define Psylc7Engine_hpp
+
+#include <stdio.h>
+
+#endif /* Psylc7Engine_hpp */
---
+++ src/Psylc7Engine.cpp
@@ -0,0 +1,8 @@
+//
+//  Psylc7Engine.cpp
+//  SDL2_App
+//
+//  Created by Le Cheng on 15/04/2020.
+//
+
+#include "Psylc7Engine.hpp"
